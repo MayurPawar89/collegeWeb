42604c76bbfdc7be734b79262a29ade059b8af66
Mayur Pawar(Office) <mayurpawar89@hotmail.com>
2015-12-07 12:57:35 +0530

    Class name changed to ITLabs.

--- collegeWebSite/collegeWebSite/Departments/IT/ITLabs.aspx.cs
+++ collegeWebSite/collegeWebSite/Departments/IT/ITLabs.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace collegeWebSite.Departments.IT
 {
-    public partial class FELabs : System.Web.UI.Page
+    public partial class ITLabs : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
