c849bab9b5bbfab56f4da88ad1c9e0fc3d26eb17
Mayur Pawar <mayurpawar89@hotmail.com>
2015-12-29 19:18:17 +0530

    Intake added for course offered

--- collegeWebSite/collegeWebSite/CoursesOffered/MBA/MBACourse.aspx.cs
+++ collegeWebSite/collegeWebSite/CoursesOffered/MBA/MBACourse.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using collegeWebSite.App_Data;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,36 @@ namespace collegeWebSite.CoursesOffered
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadDataGrid();
+            }
+        }
+        private void LoadDataGrid()
+        {
+            DataTable _dt = null;
+            _dt = GetBranchIntake();
+            grdMBAIntake.DataSource = _dt;
+            grdMBAIntake.DataBind();
+        }
+        private DataTable GetBranchIntake()
+        {
+            DataTable _dt = null;
 
+            DataAccess _DataAccess = new DataAccess();
+            StudentInformation _Student = new StudentInformation();
+            try
+            {
+                _dt = _Student.GetBranchIntakeDetails(13);
+            }
+            catch (Exception)
+            {
+                if (_DataAccess != null) { _DataAccess.RollBack(); _DataAccess.CloseConnection(false); }
+            }
+            finally
+            {
+            }
+            return _dt;
         }
     }
 }
