dcaa185ada60e163ea9226ddc56898b045f2ef1a
Mayur Pawar <mayurpawar89@hotmail.com>
2015-12-29 22:41:55 +0530

    Adding Untracked files

---
+++ collegeWebSite/collegeWebSite/Departments/ElectronicAndTeleComm/ETCExtraActivity.aspx.cs
@@ -0,0 +1,51 @@
+﻿using collegeWebSite.App_Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace collegeWebSite.Departments.ElectronicAndTeleComm
+{
+    public partial class ETCExtraActivity : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadDataGrid();
+
+            }
+        }
+
+        private void LoadDataGrid()
+        {
+            DataTable _dt = null;
+            _dt = GetGuestLectures();
+            grdETCGuestLect.DataSource = _dt;
+            grdETCGuestLect.DataBind();
+        }
+
+        private DataTable GetGuestLectures()
+        {
+            DataTable _dt = null;
+
+            DataAccess _DataAccess = new DataAccess();
+            StudentInformation _Student = new StudentInformation();
+            try
+            {
+                _dt = _Student.GetGuestLectures(6);
+            }
+            catch (Exception)
+            {
+                if (_DataAccess != null) { _DataAccess.RollBack(); _DataAccess.CloseConnection(false); }
+            }
+            finally
+            {
+            }
+            return _dt;
+        }
+    }
+}
